a54a5f780a3a87bf11c1a703a71b850363c62b7a
DeeprajB <deeprajb012@gmail.com>
2023-06-25 12:11:30 +0530

    Completed Bonus UI.

--- src/components/Bonus.js
+++ src/components/Bonus.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import { Box, Button } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import { cyan } from '@mui/material/colors';
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(cyan[200]),
+    backgroundColor: cyan[200],
+    '&:hover': {
+      backgroundColor: cyan[500],
+    },
+  }));
+
 function Bonus() {
   return (
-    <div>Bonus</div>
+    <>
+        <Box className="flex flex-col justify-center items-center my-5" sx={{
+        width: 1000,
+        backgroundColor: '#9575cd',
+        }}>
+            <p className="pt-5 text-white text-2xl font-semibold justify-center">Bonus Handler</p>
+            <div className="p-5 space-x-8">
+                <ColorButton variant="contained">Increment +</ColorButton>
+                <ColorButton variant="contained">Decrement -</ColorButton>
+            </div>
+        </Box>
+    </>
   )
 }
 
