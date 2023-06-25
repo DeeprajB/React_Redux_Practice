891fa8f8acd166d019fbdc92ace73c9857f2f89a
DeeprajB <deeprajb012@gmail.com>
2023-06-25 14:44:53 +0530

    Dsiplayed state variable instead of static.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 import Box from '@mui/material/Box';
 
@@ -6,6 +7,7 @@ import Account from '../components/Account';
 import Bonus from '../components/Bonus';
 
 function Home() {
+
   return (
     <>
         <div className="flex flex-col justify-center items-center">
@@ -16,14 +18,14 @@ function Home() {
                 backgroundColor: '#e39ff6',
                 }}>
                     <p className="p-5 text-2xl text-black font-semibold">Current Amount</p>
-                    <p className='pb-5'>NaN</p>
+                    <p className='pb-5'>{useSelector((state) => state.amount)}</p>
                 </Box>
                 <Box className="flex flex-col justify-center items-center" sx={{
                 width: '40vw',
                 backgroundColor: '#af69ef',
                 }}>
                     <p className="p-5 text-2xl text-white font-semibold">Total Bonus</p>
-                    <p className='pb-5 text-white'>NaN</p>
+                    <p className='pb-5 text-white'>{useSelector((state) => state.bonus)}</p>
                 </Box>
             </div>
             <Account />
