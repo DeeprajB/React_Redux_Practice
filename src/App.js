4a7d662f636daa85b77d656c60cdb04229ad3c3c
DeeprajB <deeprajb012@gmail.com>
2023-06-25 11:33:07 +0530

    Setup structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux';
-import { Action } from './states/reducers/index';
 
+import Home from './pages/Home'
 
 function App() {
-  const curState = useSelector((state) => state.number);
-  const dispatch = useDispatch()
   return (
     <>
-    <h1>React Redux</h1>
-    <div style={{width: '100%', display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-      <h1>{curState}</h1>
-      <div>
-        <button onClick={() => dispatch(Action(10))}>Do some Action</button>
-      </div>
-    </div>
+    <Home />
     </>
   )
 }
