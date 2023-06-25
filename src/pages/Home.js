3c93a24f1b5e79a90b734606664f9567369a5e91
DeeprajB <deeprajb012@gmail.com>
2023-06-25 11:33:56 +0530

    added components and pages.

---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Box from '@mui/material/Box';
+
+import Account from '../components/Account';
+import Bonus from '../components/Bonus';
+
+function Home() {
+  return (
+    <>
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl p-8 font-semibold" >Learn Redux with Accounts & Bonus!</h1>
+            <div className="flex flex-row">
+                <Box className="flex flex-col justify-center items-center" sx={{
+                width: 500,
+                height: 300,
+                backgroundColor: '#e39ff6',
+                }}>
+                    <p className="p-5 text-2xl text-black font-semibold">Current Amount</p>
+                    <p>NaN</p>
+                </Box>
+                <Box className="flex flex-col justify-center items-center" sx={{
+                width: 500,
+                height: 300,
+                backgroundColor: '#af69ef',
+                }}>
+                    <p className="p-5 text-2xl text-white font-semibold">Total Bonus</p>
+                    <p>NaN</p>
+                </Box>
+            </div>
+            <Account />
+            <Bonus />
+        </div>
+    </>
+  )
+}
+
+export default Home
