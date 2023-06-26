b79156b597790c5ee8db761aff5d7125d6adb5ad
DeeprajB <deeprajb012@gmail.com>
2023-06-26 10:22:40 +0530

    Completed Search Post By ID UI & Integration.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,30 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 
-import Account from '../components/Account';
-import Bonus from '../components/Bonus';
+import AllPosts from '../components/allPosts'
+import SearchPostByID from '../components/searchPostByID'
 
 function Home() {
 
   return (
     <>
         <div className="flex flex-col justify-center items-center">
-            <h1 className="text-4xl p-8 font-semibold" >Learn Redux with Accounts & Bonus!</h1>
+            <h1 className="text-4xl p-8 font-semibold" >Learn Redux with Render all Post & Search Post by ID!</h1>
             <div className="flex flex-row">
-                <Box className="flex flex-col justify-center items-center" sx={{
+                {/* <Box className="flex flex-col justify-center items-center" sx={{
                 width: '40vw',
                 backgroundColor: '#e39ff6',
                 }}>
                     <p className="p-5 text-2xl text-black font-semibold">Current Amount</p>
                     <p className='pb-5'>{useSelector((state) => state.amount)}</p>
-                </Box>
-                <Box className="flex flex-col justify-center items-center" sx={{
-                width: '40vw',
-                backgroundColor: '#af69ef',
-                }}>
-                    <p className="p-5 text-2xl text-white font-semibold">Total Bonus</p>
-                    <p className='pb-5 text-white'>{useSelector((state) => state.bonus)}</p>
-                </Box>
+                </Box> */}
             </div>
-            <Account />
-            <Bonus />
+            <SearchPostByID />
+            <AllPosts />
+            {/* <Account /> */}
+            {/* <Bonus /> */}
         </div>
     </>
   )
